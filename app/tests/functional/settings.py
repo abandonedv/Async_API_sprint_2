7b4304c775ba710b99337b01d5f5cc1178ff2621
v.ilinykh <v.ilinykh@cognitivepilot.com>
2024-03-13 17:36:15 +0300

    TestDBParams deleted

--- app/tests/functional/settings.py
+++ app/tests/functional/settings.py
@@ -1,19 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class TestDBParams(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="POSTGRES_", env_file=".env.test")
-
-    host: str
-    port: int
-    db: str
-    user: str
-    password: str
-
-    def url(self):
-        return f"{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
-
-
 class ElasticParams(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="ELASTIC_", env_file=".env.test")
 
