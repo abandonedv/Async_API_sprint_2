4ea6990773fe86cf1da074f758ae85719c1e81e6
v.ilinykh <v.ilinykh@cognitivepilot.com>
2024-03-16 23:41:59 +0300

    test_get_film_by_id_cache_request cleanup

--- app/tests/functional/src/test_film.py
+++ app/tests/functional/src/test_film.py
@@ -59,6 +59,6 @@ async def test_get_film_by_id_request(data_dict: dict, es_write_data):
     "data_dict",
     prepare_test_data(test_name="test_get_film_by_id_request"),
 )
-async def test_get_film_by_id_cache_request(data_dict: dict, es_write_data, redis):
+async def test_get_film_by_id_cache_request(data_dict: dict, es_write_data):
     await es_write_data(data_type="movies")
     await TestApiBase(**data_dict).perform(from_cache=True)
